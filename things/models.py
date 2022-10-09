f26408310cdbe68bb1af004d332fee48417f0392
Melane Navaratnarajah <melane.navaratnarajah@kcl.ac.uk>
2022-10-09 15:17:00 +0100

    Extend model thing enforcing constraints

--- things/models.py
+++ things/models.py
@@ -1,8 +1,12 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
 class Thing(models.Model):
     """Thing model class"""
 
-    name = models.CharField(max_length = 30, blank = False)
-    description = models.CharField(max_length = 100)
-    quantity = models.IntegerField(blank = False)
+    name = models.CharField(max_length = 30, blank = False, unique = True)
+    description = models.CharField(max_length = 120, unique = False, blank = False)
+    quantity = models.IntegerField(RegexValidator(
+            regex = [0-100],
+            message = 'Quantity must be a value from 0 to 100 (inclusive)'
+    ))

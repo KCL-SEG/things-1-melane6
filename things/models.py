5c492db850fd0f2e3f1d54eccf11493507beabbb
Melane Navaratnarajah <melane.navaratnarajah@kcl.ac.uk>
2022-10-09 15:28:51 +0100

    Add min and max validators for quantity

--- things/models.py
+++ things/models.py
@@ -1,12 +1,15 @@
-from django.core.validators import RegexValidator
+from django.core.validators import MaxValueValidator
+from django.core.validators import MinValueValidator
 from django.db import models
 
 class Thing(models.Model):
     """Thing model class"""
 
     name = models.CharField(max_length = 30, blank = False, unique = True)
-    description = models.CharField(max_length = 120, unique = False, blank = False)
-    quantity = models.IntegerField(RegexValidator(
-            regex = [0-100],
-            message = 'Quantity must be a value from 0 to 100 (inclusive)'
-    ))
+    description = models.CharField(max_length = 120, unique = False, blank = True)
+    quantity = models.IntegerField(
+        validators = [
+        MaxValueValidator(100,' Quantity must not be greater than 100'),
+        MinValueValidator(0,'Quantity must not be negative')
+        ]
+        )
